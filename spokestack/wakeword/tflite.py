7c395b44082f9aa517c551ce8ec62a14f49aac65
Will Rice <wrice20@gmail.com>
2020-08-25 15:27:58 -0400

    feedback changes

--- spokestack/wakeword/tflite.py
+++ spokestack/wakeword/tflite.py
@@ -5,20 +5,19 @@ the presence of keywords in an audio stream
 import os
 
 import numpy as np  # type: ignore
-from numpy import hanning
 
 from spokestack.context import SpeechContext
 from spokestack.models.tensorflow import TFLiteModel
 from spokestack.wakeword.ring_buffer import RingBuffer
 
 
-class WakewordDetector:
+class WakewordTrigger:
     """ Detects the presence of a wakeword in the audio input
 
     Args:
             pre_emphasis (float): The value of the pre-emmphasis filter
             sample_rate (int): The number of audio samples per second of audio (kHz)
-            fft_window_type (str): Window type: any that scipy.signal.get_window accepts
+            fft_window_type (str): The type of fft window. (only support for hann)
             fft_hop_length (int): Audio sliding window for STFT calculation (ms)
             model_dir (str): Path to the directory containing .tflite models
             posterior_threshold (float): Probability threshold for if a wakeword
@@ -36,12 +35,10 @@ class WakewordDetector:
     ) -> None:
 
         self.pre_emphasis: float = pre_emphasis
-        self.sample_rate: int = sample_rate
-        self.window_type: str = fft_window_type
-        if self.window_type != "hann":
-            raise ValueError("fft_window_type must be hann")
-
         self.hop_length: int = int(fft_hop_length * sample_rate / 1000)
+
+        if fft_window_type != "hann":
+            raise ValueError("Invalid fft_window_type")
 
         self.filter_model: TFLiteModel = TFLiteModel(
             model_path=os.path.join(model_dir, "filter.tflite")
@@ -49,27 +46,32 @@ class WakewordDetector:
         self.encode_model: TFLiteModel = TFLiteModel(
             model_path=os.path.join(model_dir, "encode.tflite")
         )
+        # initialize the first state input for autoregressive encoder model
         self.state = np.zeros(self.encode_model.input_details[1]["shape"], np.float32)
         self.detect_model: TFLiteModel = TFLiteModel(
             model_path=os.path.join(model_dir, "detect.tflite")
         )
-        self.window_size: int = (
-            self.filter_model.input_details[0]["shape"][-1] - 1
-        ) * 2
-        self._hann_window = hanning(self.window_size)
+
+        # window size calculated based on fft
+        # the filter inputs are (fft_size - 1) / 2
+        # which makes the window size (post_fft_size - 1) * 2
+        self._window_size = (self.filter_model.input_details[0]["shape"][-1] - 1) * 2
+        self._fft_window = np.hanning(self._window_size)
         self.mel_length: int = self.encode_model.input_details[0]["shape"][1]
         self.mel_width: int = self.encode_model.input_details[0]["shape"][-1]
         self.encode_length: int = self.detect_model.input_details[0]["shape"][1]
         self.encode_width: int = self.detect_model.input_details[0]["shape"][-1]
 
-        self.sample_window: RingBuffer = RingBuffer(shape=[self.window_size])
+        self.sample_window: RingBuffer = RingBuffer(shape=[self._window_size])
         self.frame_window: RingBuffer = RingBuffer(
             shape=[self.mel_length, self.mel_width]
         )
         self.encode_window: RingBuffer = RingBuffer(
             shape=[self.encode_length, self.encode_width]
         )
+        # initialize frame window with zeros
         self.frame_window.fill(0.0)
+        # initialize encoder window with zeros
         self.encode_window.fill(0.0)
         self._posterior_threshold: float = posterior_threshold
         self._posterior_max: float = 0.0
@@ -86,58 +88,80 @@ class WakewordDetector:
         Returns: None
 
         """
+        # detect vad edges for wakeword deactivation
         vad_fall = self._is_speech and not context.is_speech
         self._is_speech = context.is_speech
+
+        # sample frame to detect the presence of wakeword
         if not context.is_active:
             self._sample(context, frame)
 
+        # reset on vad fall deactivation
         if vad_fall:
             self.reset()
 
     def _sample(self, context: SpeechContext, frame) -> None:
-
+        # normalize incoming audio to (-1.0, 1.0)
         frame = frame.astype(np.float32) / (2 ** 15 - 1)
+        # ensure range does not exceed (-1.0, 1.0)
         frame = np.clip(frame, -1.0, 1.0)
+        # pull out a single value from the frame
         prev_sample = frame[-1]
+        # apply pre-emphasis with the previous sample
         frame -= self.pre_emphasis * np.append(self._prev_sample, frame[:-1])
+        # cache the previous sample to be use in the next iteration
         self._prev_sample = prev_sample
 
         for sample in frame:
-
             self.sample_window.write(sample)
-
+            # fill the sample window
             if self.sample_window.is_full:
+                # run analyze if speech detected in the vad
                 if context.is_speech:
                     self._analyze(context)
+                # prepare the sample window for the next iteration
                 self.sample_window.rewind().seek(self.hop_length)
 
     def _analyze(self, context: SpeechContext) -> None:
+        # read the full contents of the sample window
         frame = self.sample_window.read_all()
-        frame = np.fft.rfft(frame * self._hann_window, n=self.window_size)
+        # apply real valued fft
+        frame = np.fft.rfft(frame * self._fft_window, n=self._window_size)
+        # polarize and cast from float64 to float32
         frame = np.abs(frame).astype(np.float32)
+        # compute mel spectrogram
         self._filter(context, frame)
 
     def _filter(self, context: SpeechContext, frame) -> None:
+        # add the batch dimension
         frame = np.expand_dims(frame, 0)
+        # compute the mel spectrogram with filter model
         frame = self.filter_model(frame)[0]
+        # write to the frame window
         self.frame_window.rewind().seek(1)
         self.frame_window.write(frame)
+        # encode the mel spectrogram
         self._encode(context)
 
     def _encode(self, context: SpeechContext) -> None:
+        # read the full contents of the frame window
         frame = self.frame_window.read_all()
+        # add batch dimension
         frame = np.expand_dims(frame, 0)
+        # run the encoder and save the output state for autoregression
         frame, self.state = self.encode_model(frame, self.state)
-
+        # accumulate encoded samples until size of detection window
         self.encode_window.rewind().seek(1)
         self.encode_window.write(frame)
-
         self._detect(context)
 
     def _detect(self, context: SpeechContext) -> None:
+        # read the full contents of the encode window
         frame = self.encode_window.read_all()
+        # add the batch dimension
         frame = np.expand_dims(frame, 0)
-        posterior = self.detect_model(frame)[0]
+        # get scalar probability of wakeword
+        posterior = self.detect_model(frame)[0][0][0]
         if posterior > self._posterior_threshold:
             context.is_active = True
         if posterior > self._posterior_max:
--- tests/wakeword/test_tflite.py
+++ tests/wakeword/test_tflite.py
@@ -7,31 +7,38 @@ import numpy as np  # type: ignore
 import pytest  # type: ignore
 
 from spokestack.context import SpeechContext
-from spokestack.wakeword.ring_buffer import RingBuffer
-from spokestack.wakeword.tflite import WakewordDetector
+from spokestack.wakeword.tflite import WakewordTrigger
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+class ModelFactory(mock.MagicMock):
+    def __call__(self, model_path):
+        model = mock.MagicMock()
+        if model_path.endswith("filter.tflite"):
+            model.input_details = [{"shape": [1, 257]}]
+            model.output_details = [{"shape": [1, 40]}]
+            model.return_value = [np.zeros((1, 40))]
+        elif model_path.endswith("encode.tflite"):
+            model.input_details = [{"shape": [1, 1, 40]}, {"shape": [1, 128]}]
+            model.output_details = [{"shape": [1, 128]}, {"shape": [1, 128]}]
+            model.return_value = [np.zeros((1, 128)), np.zeros((1, 128))]
+        elif model_path.endswith("detect.tflite"):
+            model.input_details = [{"shape": [1, 100, 128]}]
+            model.output_details = [{"shape": [1, 1]}]
+            model.return_value = [np.zeros((1, 1))]
+        return model
+
+
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_invalid_args(_mock):
     with pytest.raises(ValueError):
-        _ = WakewordDetector(fft_window_type="hamming")
+        _ = WakewordTrigger(fft_window_type="hamming")
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_vad_inactive(_mock):
     context = SpeechContext()
 
-    with mock.patch("spokestack.wakeword.tflite.hanning"):
-        detector = WakewordDetector(model_dir="wakeword_model")
-
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[0.0])
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
+    detector = WakewordTrigger(model_dir="wakeword_model")
 
     test_frame = np.random.rand(160,).astype(np.float32)
     context.is_speech = False
@@ -39,23 +46,11 @@ def test_detect_vad_inactive(_mock):
     assert not context.is_active
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_vad_active(_mock):
     context = SpeechContext()
 
-    with mock.patch("spokestack.wakeword.tflite.hanning"):
-        detector = WakewordDetector(model_dir="wakeword_model")
-
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[0.6])
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
-
-    detector.state = np.zeros((1, 128))
+    detector = WakewordTrigger(model_dir="wakeword_model")
 
     for _ in range(1):
         test_frame = np.random.rand(160,).astype(np.float32)
@@ -66,19 +61,10 @@ def test_detect_vad_active(_mock):
     detector.reset()
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_inactive_vad_deactivate(_mock):
     context = SpeechContext()
-    with mock.patch("spokestack.wakeword.tflite.hanning"):
-        detector = WakewordDetector(model_dir="wakeword_model")
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[0.0])
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
+    detector = WakewordTrigger(model_dir="wakeword_model")
 
     for _ in range(3):
         test_frame = np.random.rand(160,).astype(np.float32)
@@ -88,22 +74,11 @@ def test_detect_inactive_vad_deactivate(_mock):
         assert not context.is_active
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_activate(_mock):
     context = SpeechContext()
-    with mock.patch(
-        "spokestack.wakeword.tflite.hanning", return_value=np.zeros((512,))
-    ):
-        detector = WakewordDetector(model_dir="wakeword_model")
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[0.6])
-    detector.window_size = 512
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
+    detector = WakewordTrigger(model_dir="wakeword_model")
+    detector.detect_model.return_value[0][:] = 0.6
 
     test_frame = np.random.rand(512,).astype(np.float32)
     context.is_speech = True
@@ -112,28 +87,16 @@ def test_detect_activate(_mock):
     assert context.is_active
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_active_min_delay(_mock):
     context = SpeechContext()
-    with mock.patch(
-        "spokestack.wakeword.tflite.hanning", return_value=np.zeros((512,))
-    ):
-        detector = WakewordDetector(model_dir="wakeword_model")
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[0.0])
-    detector.window_size = 512
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
+    detector = WakewordTrigger(model_dir="wakeword_model")
 
     test_frame = np.random.rand(512,).astype(np.float32)
     context.is_speech = True
     detector(context, test_frame)
 
-    detector.detect_model.return_value = [1.0]
+    detector.detect_model.return_value[0][:] = 1
     detector(context, test_frame)
 
     context.is_speech = False
@@ -142,22 +105,11 @@ def test_detect_active_min_delay(_mock):
     assert context.is_active
 
 
-@mock.patch("spokestack.models.tensorflow.tflite")
+@mock.patch("spokestack.wakeword.tflite.TFLiteModel", new_callable=ModelFactory)
 def test_detect_manual_min_delay(_mock):
     context = SpeechContext()
-    with mock.patch(
-        "spokestack.wakeword.tflite.hanning", return_value=np.zeros((512,))
-    ):
-        detector = WakewordDetector(model_dir="wakeword_model")
-
-    detector.encode_model = mock.MagicMock(
-        return_value=[np.ones((1, 1, 128)), np.zeros((1, 128))]
-    )
-    detector.filter_model = mock.MagicMock(return_value=[np.ones((1, 40))])
-    detector.detect_model = mock.MagicMock(return_value=[1.0])
-    detector.sample_window = RingBuffer([512])
-    detector.frame_window = RingBuffer([1, 40])
-    detector.encode_window = RingBuffer([1, 100, 128])
+    detector = WakewordTrigger(model_dir="wakeword_model")
+    detector.detect_model.return_value[0][:] = 1
 
     context.is_active = True
     test_frame = np.random.rand(512,).astype(np.float32)
