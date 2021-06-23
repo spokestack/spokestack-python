099f415cb5eb39f4ed7e30e1c46440f96ed041ae
Will Rice <will@spokestack.io>
2021-06-23 13:38:45 -0400

    Increment version to 0.0.22

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spokestack",
-    version="0.0.21",
+    version="0.0.22",
     author="Spokestack",
     author_email="support@spokestack.io",
     description="Spokestack Library for Python",
