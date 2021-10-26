95e451f9ab6ab1af2370d3e1007ebf6739e4765f
Will Rice <will@spokestack.io>
2021-10-26 16:43:55 -0400

    v0.0.23 (#105)

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spokestack",
-    version="0.0.22",
+    version="0.0.23",
     author="Spokestack",
     author_email="support@spokestack.io",
     description="Spokestack Library for Python",
